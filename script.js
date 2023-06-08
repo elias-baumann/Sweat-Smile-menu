f7e3092f1d8f1b872109099e920033f80dfd8fd2
Elias <52203697+elias-baumann@users.noreply.github.com>
2023-06-08 14:33:57 +0200

    added files

---
+++ script.js
@@ -0,0 +1,7 @@
+function openLeft() {
+  window.location.href = "https://elias-baumann.github.io/Sweat-Smile-1/";
+}
+
+function openRight() {
+  window.location.href = "https://elias-baumann.github.io/Sweat-Smile-2/";
+}
